ed6622f6a9f64ab9278d564150f1b340ec5365e2
musyahya <mananabdul37654@gmail.com>
2021-08-16 09:04:43 +0700

    getByTodolistId

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\StepController;
 use App\Http\Controllers\TodolistController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -20,3 +21,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::resource('/todolist', TodolistController::class);
+Route::resource('/step', StepController::class);
+Route::get('/step/{id}/getByTodolistId', [StepController::class, 'getByTodolistId']);
